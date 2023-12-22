c0434bc073861f2db44bd7e5b1a91fba77161064
Gernot Steinegger <Gernot.Steinegger@gmail.com>
2023-12-22 11:00:40 +0000

    feat: prettier and eslint

--- src/server/threeServer.ts
+++ src/server/threeServer.ts
@@ -1,26 +1,27 @@
-import express from 'express'
-import path from 'path'
-import http from 'http'
+import express from 'express';
+import path from 'path';
+import http from 'http';
 
-const port: number = 3000
-const hostname:string = '127.0.0.1'
+const port: number = 3000;
+const hostname: string = '127.0.0.1';
 
 class App {
-    private server: http.Server
-    private port: number
+  private server: http.Server;
+  private port: number;
 
-    constructor(port: number) {
-        this.port = port
-        const app = express()
-        app.use(express.static(path.join(__dirname, '../client')))
-        this.server = new http.Server(app)
-    }
+  constructor(portNumber: number) {
+    this.port = portNumber;
+    const app = express();
+    // eslint-disable-next-line no-undef
+    app.use(express.static(path.join(__dirname, '../client')));
+    this.server = new http.Server(app);
+  }
 
-    public Start() {
-        this.server.listen(this.port, () => {
-            console.log(`Server running at http://${hostname}:${port}/`)
-        })
-    }
+  public Start() {
+    this.server.listen(this.port, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+    });
+  }
 }
 
-new App(port).Start()
+new App(port).Start();
